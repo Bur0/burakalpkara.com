cf121ef0cfde86314d2c359bc3fa258b786a8f8f
Bur0 <burakalpkara@gmail.com>
2020-09-20 23:54:42 +0300

    website config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,45 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `Burak Alp KARA`,
+    name: `Burak Alp`,
+    siteUrl: `https://burakalpkara.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-      maxWidth: 652,
+      heading: `Welcome to area.`,
+      maxWidth: 652
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/burakalpk`
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/Bur0`
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/burakalpkara.com`
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
-    ],
+        url: `https://www.linkedin.com/in/burakalpkara`
+      }
+    ]
   },
   plugins: [
     {
-      resolve: "@narative/gatsby-theme-novela",
+      resolve: '@narative/gatsby-theme-novela',
       options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
+        contentPosts: 'content/posts',
+        contentAuthors: 'content/authors',
+        basePath: '/',
         authorsPage: true,
         sources: {
-          local: true,
+          local: true
           // contentful: true,
-        },
-      },
+        }
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -54,13 +50,12 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `standalone`,
-        icon: `src/assets/favicon.png`,
-      },
+        icon: `src/assets/favicon.png`
+      }
     },
     {
       resolve: `gatsby-plugin-netlify-cms`,
-      options: {
-      },
-    },
-  ],
-};
+      options: {}
+    }
+  ]
+}
